a5725f1917dab01f43f16fc74da033ba000da582
sah420 <salemabuhassan80@gmail.com>
2022-02-24 18:05:24 +0000

    added stack code

---
+++ stack.py
@@ -0,0 +1,36 @@
+from node import Node
+
+class Stack:
+    def __init__(self, name):
+        self.size = 0
+        self.top_item = None
+        self.limit = 1000
+        self.name = name
+        
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            print("No more room!")
+            
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        print("This stack is totally empty.")
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.top_item.get_value()
+        print("Nothing to see here!")
+        
+    def has_space(self):
+        return self.limit > self.size
+        
+    def is_empty(self):
+        return self.size == 0
